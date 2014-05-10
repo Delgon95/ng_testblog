13f7e4d0a0c85a346e2905e87859e48819fd3b23
Michal Zurkowski <michal.zurkowski@gmail.com>
2014-05-10 16:05:29 +0200

    Creating comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,25 @@
 class CommentsController < ApplicationController
+  before_filter :authenticate_user!
+  expose(:post)
+  expose(:comment, attributes: :comment_params)
+
+  def new
+  end
+
+  def create
+    if comment.save
+      redirect_to post_path(post)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body).merge(
+      user_id: current_user.id,
+      post_id: post.id
+    )
+  end
 end
