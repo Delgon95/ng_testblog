dae72a22a9908ab759ad29f05cfd7bfe62934448
Michal Zurkowski <michal.zurkowski@gmail.com>
2014-05-10 16:31:03 +0200

    Add hotness method

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,4 +18,24 @@ class Post
     update_attribute :archived, true
   end
 
+  def hotness
+   age_score + (commented_by(3) ? 1 : 0)
+  end
+
+  private
+
+  def age_score
+    return 0 if age > 7.days
+    return 1 if age > 3.days
+    return 2 if age > 1.day
+    return 3
+  end
+
+  def age
+    Time.now - created_at
+  end
+
+  def commented_by amount
+    comments.count >= amount
+  end
 end
