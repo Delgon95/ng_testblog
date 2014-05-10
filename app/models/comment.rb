d5b532003da85f92e6048ab72ade57f4ebbdd6fa
Michal Zurkowski <michal.zurkowski@gmail.com>
2014-05-10 13:56:04 +0200

    Add Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,11 @@
 class Comment
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :body, type: String
+
+  validates_presence_of :body
+
+  belongs_to :post
+  belongs_to :user
 end
